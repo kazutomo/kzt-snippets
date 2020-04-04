c053fac3eeb093e926fb608019a55d3f49c0f950
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-04-04 16:34:36 -0500

    added Array matching

--- scala/PatternMatch.scala
+++ scala/PatternMatch.scala
@@ -14,4 +14,16 @@ object PatternMatch extends App {
   val l = List[Any](1.toByte, 2.toShort, 3, 4.toLong, 5.5.toFloat, 6.6)
 
   l foreach{ v => println( sizeof(v) ) }
+
+
+  // Note: this only works for Array, not List due to the erasure rule
+  def is2DIntArray(x: Any) = x match {
+    case a: Array[Array[Int]] => "yes"
+    case _ => "no"
+  }
+
+  val l2 = List[Any]( Array(1,2,3),
+    Array[Array[Int]]( Array(1,2), Array(3,4)) )
+
+  l2 foreach{ v => println( is2DIntArray(v) ) }
 }
