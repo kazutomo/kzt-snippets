fce3a2cc3873c4d4a4d77db02937e9733583a54d
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-04-11 22:05:49 -0500

    another usage

--- scala/RepeatedParams.scala
+++ scala/RepeatedParams.scala
@@ -3,10 +3,14 @@ object RepeatedParams extends App {
 
   def foo(s: String*) = for (i <- s) println(i)
 
+  def bar(s: String*) : List[String] = s.toList
+
   val str = "blue in green"
   val star = str.split(" ")
 
   foo("kind", "of", "blue")
 
   foo(star: _*)  // to convert it to repeated args
+
+  println( bar(start: _*) )
 }
