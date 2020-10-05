71de3a51f7efc9e6c44fde3c496e1a798247aaab
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-10-04 19:45:09 -0500

    just a comment

--- chisel/XnorPop.scala
+++ chisel/XnorPop.scala
@@ -3,6 +3,7 @@ package foobar
 import chisel3._
 import chisel3.util._  // to use PopCount and log2Ceil
 
+// An implementation of XnorPop count operation used in Binarized Neural Network
 class XnorPop(val ninputs:Int = 8) extends Module {
   val io = IO(new Bundle {
     val vin     = Input( UInt(ninputs.W))
@@ -13,6 +14,7 @@ class XnorPop(val ninputs:Int = 8) extends Module {
   for (i <- 0 until ninputs) {
     val tmp = PopCount( ~(io.vin ^ io.weights(i)) ).asSInt
 
+    // the result is the number between +N and -N
     io.vout(i) := tmp - (ninputs >> 1).S
   }
 }
