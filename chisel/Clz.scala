3f29053a6726dceb79058490e5fcf3de38e9f0ca
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-11-26 19:50:15 -0600

    comments and improved testbench

--- chisel/Clz.scala
+++ chisel/Clz.scala
@@ -23,31 +23,53 @@ class Clz2() extends Module {
 // A parameterized version of counting leading-zeros
 // note: nb is the power of two number that is greater-equal than 4.
 class ClzParam(nb: Int = 16) extends Module {
+
   val half = nb >> 1
   val lognb = log2Ceil(nb)
 
   val io = IO(new Bundle {
-    val in = Input(UInt(nb.W))
+    // input data
+    val in  = Input(UInt(nb.W))
+    // the number of the leading zeros (Nlz) of the input data, which
+    // requires (log2Ceil(nb)+1) bits to hold the number. e.g., a
+    // 16-bit input, the number of the leading-zeros is between 0 and
+    // 16 (all bits are zeros), which requires 5 bits.
     val out = Output(UInt((lognb+1).W))
   })
 
+  // divide 'in' into two blocks: c0 (lower half) and c1 (upper half)
+  // when the half size is 2, instantiate Clz2(), otherwise
+  // ClzParam(), the same class
   val c0 = if (half==2) Module(new Clz2()) else Module(new ClzParam(half))
   val c1 = if (half==2) Module(new Clz2()) else Module(new ClzParam(half))
-
   c0.io.in := io.in(half-1, 0)
   c1.io.in := io.in(nb-1  , half)
 
   io.out := 0.U((lognb+1).W)
+
+  // If both two divided blocks only contain zero or MSB of the
+  // counting number of both blocks are one, the original input 'in'
+  // should only contain zero.
   when( c1.io.out(lognb-1) && c0.io.out(lognb-1) ) {
+    // All-zero case. simple set MSB of io.out one.
     io.out := Cat("b1".U, 0.U(lognb.W))
   } .elsewhen( c1.io.out(lognb-1) === 0.U ) {
+    // the upper half contains one or more 1, so 'io.out' is simply
+    // Nlz of the upper half, regardless of the content of the lower
+    // half.
     io.out := Cat("b0".U, c1.io.out)
   } .elsewhen( c1.io.out(lognb-1) === 1.U ) {
+    // the upper half only contain zero while the lower half is not
+    // all-zero. 'io.out' is the upper half Nlz plus the lower half
+    // Nlz.
     io.out := Cat("b01".U, c0.io.out(lognb-2,0))
   }
 }
 
-// below are old hard-coded version
+
+//
+// below are old hard-coded version. unused.
+//
 
 class Clz4() extends Module {
   val io = IO(new Bundle {
--- chisel/ClzTests.scala
+++ chisel/ClzTests.scala
@@ -5,16 +5,35 @@ import chisel3.iotesters.PeekPokeTester
 
 class ClzUnitTester(c: ClzParam) extends PeekPokeTester(c) {
 
-  // val nb = c.nb
+  val nb = 16 // c.nb // the size of input data in bits
 
-  val h = 1<<15
+  val seed = 123
+  val rn = new scala.util.Random(seed)
 
-  for (s <- 0 until 17) {
-    val in = h >> s
+  def genNumber() : (Long, Int) = {
+    val sh = rn.nextInt(nb+1) // random number between 0 and nb
+    val b = if (sh == nb) 0.toLong else 1.toLong << sh
+    val l = if (b < 2.toLong ) 0.toLong else rn.nextLong().abs % b
+    (b + l, sh)
+  }
+  val ntries = 100
+
+  def toBinStr(v: Long): String = {
+    val s = v.toBinaryString
+    val zslen = nb - s.length()
+    val zs = "0" * zslen
+    zs + s
+  }
+
+  for (s <- 0 until ntries) {
+    val (in, sh) = genNumber()
+    val str = toBinStr(in)
+    val ref = if (sh == nb) nb else (nb-sh-1)
+    print("in=" + str)
     poke(c.io.in, in)
-    expect(c.io.out, s)
+    expect(c.io.out, ref)
     val out = peek(c.io.out)
-    print(f"in=$in%04x  out=$out\n")
+    print(f"  out=$out\n")
     step(1)
   }
 }
