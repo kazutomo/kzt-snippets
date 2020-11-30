e5900110e57c69c0513ea72e0f5062ea47725021
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2020-11-29 20:26:53 -0600

    restructure dut and tester driver so that made adding a new dut/tester slightly easier

--- chisel/TestDriver.scala
+++ chisel/TestDriver.scala
@@ -9,73 +9,95 @@ import chisel3.iotesters
 import chisel3.iotesters.{Driver, PeekPokeTester}
 
 object TestMain extends App {
-  // component target list.
+  // default params and component target list
   val targetlist = List(
-    "rev", "concat", "xnor", "clz", "srmem", "nerd", "count"
-  )
+    "rev", "foo", "count",
+    "concat", "xnor", "clz", "srmem", "nerd", "count")
+
 
   val a = if (args.length > 0) args(0) else "rev"
   val tmp = a.split(":")
-
   val target = tmp(0)
   val mode = if (tmp.length > 1) tmp(1) else "test"
+
+  if ( target=="list" ) {
+    println("*target list")
+    for (t <- targetlist)  println(t)
+    System.exit(0)
+  }
 
   mode match {
     case "test" => {}
     case "verilog" => {}
-    case _ => println(f"Warning: $mode is not a valid mode")
+    case _ => {
+      println(f"Warning: $mode should be 'test' or 'verilog'")
+      System.exit(1)
+    }
   }
-
   println(f"MODE=$mode TARGET=$target")
 
-  // TODO: make this block concise since there are lots of repeating here. 
   target match {
     case "list" =>
       println("*target list")
       for (t <- targetlist)  println(t)
     case "rev" =>
       val bitwidth = 8
+      val dut = () => new Rev(bitwidth)
+      val tester = c => new RevUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new Rev(bitwidth))
-        case _ => iotesters.Driver.execute(args, () => new Rev(bitwidth)) {c => new RevUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "foo" =>
+      val dut = () => new Foo()
+      val tester = c => new FooUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new Foo())
-        case _ => iotesters.Driver.execute(args, () => new Foo()) {c => new FooUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "count" =>
+      val dut = () => new Counter()
+      val tester = c => new CounterUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new Counter())
-        case _ => iotesters.Driver.execute(args, () => new Counter()) {c => new CounterUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "nerd" =>
+      val dut = () => new NerdCounter()
+      val tester = c => new NerdCounterUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new NerdCounter())
-        case _ => iotesters.Driver.execute(args, () => new NerdCounter()) {c => new NerdCounterUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
-
     case "xnor" =>
       val ninputs = 8
+      val dut = () => new XnorPop(ninputs)
+      val tester = c => new XnorPopUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new XnorPop(ninputs))
-        case _ => iotesters.Driver.execute(args, () => new XnorPop(ninputs)) {c => new XnorPopUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "clz" =>
       val nb = 16
+      val dut = () => new ClzParam(nb)
+      val tester = c => new ClzUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new ClzParam(nb))
-        case _ => iotesters.Driver.execute(args, () => new ClzParam(nb)) {c => new ClzUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "srmem" =>
+      val dut = () => new SRMem()
+      val tester = c => new SRMemUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new SRMem())
-        case _ => iotesters.Driver.execute(args, () => new SRMem()) {c => new SRMemUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
     case "concat" =>
+      val dut = () => new ConcatVecs()
+      val tester = c => new ConcatVecsUnitTester(c)
       mode match {
-        case "verilog" => chisel3.Driver.execute(args, () => new ConcatVecs())
-        case _ => iotesters.Driver.execute(args, () => new ConcatVecs()) {c => new ConcatVecsUnitTester(c) }
+        case "verilog" => chisel3.Driver.execute(args, dut)
+        case _ => iotesters.Driver.execute(args, dut) {tester}
       }
   }
 }
