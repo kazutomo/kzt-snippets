cf9c39c8136ff6e9ef4e7f312393a6a8e2ff95fa
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2021-04-25 22:27:37 -0400

    added test

--- chisel/HoldTest.scala
+++ chisel/HoldTest.scala
@@ -8,19 +8,41 @@ import testutil._
 class HoldUnitTester(c: Hold) extends PeekPokeTester(c) {
 
   val bw = c.bw
+  val maxval = (1<<bw)-1
   val n = c.n
-  val ntests = 20
+  val seed = 123
+  val r = new scala.util.Random(seed)
+  val ntests = 10
+  val testdata = List.tabulate(ntests*n) {i => r.nextInt(maxval)}
+  val holdbuf = new Array[Int](n)
+  var pos = 0
 
-  for (i <- 0 until ntests) {
-    val in = i+1
-    poke(c.io.d_in, in)
 
-    val outstr = List.tabulate(n) {i => TestUtil.convIntegerToHexStr(peek(c.io.d_out(i)), bw*n) } reduce(_+_)
-    val valid = peek(c.io.d_out_valid)
-    printf(f"${i}%2d: ${in}%02x => $outstr $valid\n")
+  def test(indata: Int) {
+    val outstr = List.tabulate(n) {i => TestUtil.convIntegerToHexStr(peek(c.io.d_out(i)), bw) } reduce(_+_)
+    printf(f"${indata}%02x => $outstr ")
+
+    if (peek(c.io.d_out_valid)>0) {
+      for (i <- 0 until n) expect(c.io.d_out(i), holdbuf(i))
+      val validated = List.tabulate(n){i => peek(c.io.d_out(i)).toInt == holdbuf(i)} reduce(_&_)
+      printf(f"validated=${validated}")
+    }
+    printf("\n")
+  }
+
+  for (indata <- testdata) {
+    poke(c.io.d_in, indata)
+
+    test(indata)
+
+    holdbuf(pos) = indata
+    if (pos<n-1)   pos += 1
+    else pos = 0
 
     step(1)
   }
+
+  test(0)
 }
 
 object HoldTest {
