75633895ecad79a1688562b71c8984de4a49f373
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2021-05-29 21:39:01 -0500

    simplify counter

--- chisel/Counter.scala
+++ chisel/Counter.scala
@@ -6,16 +6,14 @@ import chisel3.util._  // to use PopCount and log2Ceil
 class Counter(val max:Int = 10) extends Module {
   val nbits = log2Ceil(max+1)
   val io = IO(new Bundle {
+    val enable = Input(Bool())
     val out = Output(UInt(nbits.W))
   })
 
   val cntReg = RegInit(0.U(nbits.W))
 
-  cntReg := cntReg + 1.U
-
-  when( cntReg === (max-1).U) {
-    cntReg := 0.U
-  }
+  cntReg := Mux(cntReg === max.U, 0.U,
+    Mux(io.enable, cntReg + 1.U, cntReg))
 
   io.out := cntReg
 }
--- chisel/CounterTest.scala
+++ chisel/CounterTest.scala
@@ -8,19 +8,26 @@ import testutil._
 class CounterUnitTester(c: Counter) extends PeekPokeTester(c) {
 
   val max = c.max
+  var refcnt = 0
 
-  val ntests = 10
-
-  for (i <- 0 until max*2 + 2) {
-    expect(c.io.out, i % max)
-    printf("%08d => %08d\n",  i, peek(c.io.out).toInt)
-    step(1)
+  def test(e : Int) {
+    poke(c.io.enable, e)
+    for (i <- 0 until max+2) {
+      expect(c.io.out, refcnt)
+      printf("out=%2d ref=%2d enable=%d\n", peek(c.io.out).toInt, refcnt, e)
+      step(1)
+      if (e==1) {
+        if(refcnt < max) refcnt += 1 else refcnt = 0
+      }
+    }
   }
+  test(1)
+  test(0)
 }
 
 object CounterTest {
   def run(args: Array[String]) {
-    val maxval = 10
+    val maxval = 9
     val dut = () => new Counter(maxval)
     val tester = c => new CounterUnitTester(c)
 
